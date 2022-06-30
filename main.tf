b3fced1c261299bfc8b538554207928bcf70491a
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2022-06-30 02:27:42 +0100

    Replace placeholder resource

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ terraform {
 
 provider "pagerduty" {}
 
-resource "pagerduty_team" "engineering" {
-  name        = "Engineering"
-  description = "All engineering"
+resource "pagerduty_business_service" "example" {
+  name             = "My Web App"
+  description      = "A very descriptive description of this business service"
+  point_of_contact = "PagerDuty Admin"
 }
